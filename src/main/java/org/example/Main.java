bbc936d044b8b9600d5bdc76be16f002ca4cd7dd
Ернар <230709@astanait.edu.kz>
2024-05-10 22:32:12 +0500

    Selection sort

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,23 +2,25 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void insertionSort(int[] array) {
+    public static void selectionSort(int[] array) {
         int n = array.length;
-        for (int i = 1; i < n; ++i) {
-            int key = array [i];
-            int j =   i - 1;
 
-            while (j >= 0 && array[j] > key) {
-                array[j + 1] = array [j];
-                j--;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
             }
-            array [j + 1] = key;
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
         }
     }
     public static void main(String[] args) {
         int[] array = {12, 11, 13, 5, 6, 7};
 
-        insertionSort(array);
+        selectionSort(array);
         printArr(array);
     }
     public static void printArr(int[] arr)
