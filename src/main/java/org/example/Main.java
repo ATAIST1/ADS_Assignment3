097ac266669606ad5ffd8679741033bead6e357b
Ернар <230709@astanait.edu.kz>
2024-05-10 21:44:03 +0500

    Insertion Sort

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,47 +1,24 @@
 package org.example;
-
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void heapSort(int[] array) {
+    public static void insertionSort(int[] array) {
         int n = array.length;
+        for (int i = 1; i < n; ++i) {
+            int key = array [i];
+            int j =   i - 1;
 
-        for (int i = n / 2 - 1; i >= 0; i--) {
-            heapify(array, n, i);
-        }
-
-        for (int i = n - 1; i > 0; i--) {
-            int temp = array[0];
-            array[0] = array[i];
-            array[i] = temp;
-
-            heapify(array, i, 0);
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array [j];
+                j--;
+            }
+            array [j + 1] = key;
         }
     }
-
-    private static void heapify(int[] array, int n, int i) {
-        int largest = i;
-        int leftChild = 2 * i + 1;
-        int rightChild = 2 * i + 2;
-        if (leftChild < n && array[leftChild] > array[largest]) {
-            largest = leftChild;
-        }
-        if (rightChild < n && array[rightChild] > array[largest]) {
-            largest = rightChild;
-        }
-        if (largest != i) {
-            int swap = array[i];
-            array[i] = array[largest];
-            array[largest] = swap;
-            heapify(array, n, largest);
-        }
-    }
-
     public static void main(String[] args) {
         int[] array = {12, 11, 13, 5, 6, 7};
 
-        heapSort(array);
+        insertionSort(array);
         printArr(array);
     }
     public static void printArr(int[] arr)
